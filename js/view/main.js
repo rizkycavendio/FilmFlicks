feed0f26c7eee14bac8aee1cebcaef9b297fe5b6
rizkycavendio <mrc.dio2001@gmail.com>
2023-09-28 22:59:39 +0700

    film-title css, es6 style in main.js

--- js/view/main.js
+++ js/view/main.js
@@ -3,7 +3,7 @@ import films from '../data/films.js';
 const renderFilm = () => {
     var filmListDiv = document.getElementById("film-list");
 
-    films.forEach(function(film) {
+    films.forEach((film) => {
         var filmItemDiv = document.createElement("div");
         filmItemDiv.className = "film-item";
 
@@ -15,7 +15,7 @@ const renderFilm = () => {
             <img src="${film.gambar}" alt="${film.judul}">
             <h2 class="film-title">${film.judul}</h2>
             <p class="film-genre">${film.genre.join(", ")}</p>
-            <p class="film-description">${film.deskripsi}</p>
+            <p class="film-description">${film.deskripsi || 'Belum ada deskripsi'}</p>
         `;
 
         filmListDiv.appendChild(filmItemDiv);
